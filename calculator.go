1596250a235eadc2a54c1c0ff1ed387fd2c38234
raushan211 <raazcrzy90@gmail.com>
2022-06-08 17:37:11 +0530

    calculator

--- calculator.go
+++ calculator.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	num1 := 100
-	num2 := 500
+	fmt.Println("num1: ")
+	var num1 int
+	fmt.Scanln(&num1)
+	fmt.Println("num2: ")
+	var num2 int
+	fmt.Scanln(&num2)
+	fmt.Println("operation: ")
+	var third string
+	fmt.Scanln(&third)
+
 	result := 0
 	operation := "+"
 	if operation == "+" {
